9664f5faa37c268c240b826b0f84c5444aa1fb43
ZhanelKaiyr <53339773+ZhanelKaiyr@users.noreply.github.com>
2019-11-21 09:41:08 +0600

    Add files via upload

--- 49.cpp
+++ 49.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    int a,b,c,d,max=0;
-    cin >>a>>b>>c>>d;
-    if (a%2==0||b%2==0||c%2==0||d%2==0){
-        if (a%2==0)
-            max = a;
-         if (b%2 == 0 && b>max)
-            max = b;
-         if (c%2 == 0 && c>max)
-            max = c;
-         if (d%2 == 0 && d>max)
-            max = d;
-    }
-     else {
-        cout <<"Not found";
-    }
-    cout << max;
+    int a,b,c,d,maxi=0;
+    cin>>a>>b>>c>>d;
+    if(a>=b && a>=c && a>=d && a%2==0)
+        maxi = a;
+    else if(b>=a && b>=c && b>=d && b%2==0)
+        maxi = b;
+    else if(c>=b && c>=a && c>=d && c%2==0)
+        maxi = c;
+    else if(d>=b && d>=c && d>=a && d%2==0)
+        maxi = d;
+    else
+        {
+            cout <<"Not found"<<endl;
+        }
+    cout << maxi;
 }
